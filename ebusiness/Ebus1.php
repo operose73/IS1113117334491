1fa74a0a7bbad9ca485bb1aeafa78948d048a792
Andy <117334491@umail.ucc.ie>
2018-03-08 13:54:19 +0000

    Calculations

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -16,40 +16,78 @@
             <br/>
             
             <form method="POST" action="Ebus2.php">
-              
-              <label for="salesforce">
+
+            <!-- Adding a radio button for salesforce -->
+            <label for="salesforce">
                 <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
-                SalesForce @ $100
-              </label>
-              
-              <br/>
-              
-              <label for="aws">
+                SaleForce @ $100
+            </label>
+            <br/>
+            <br/>
+            
+            
+            <!-- Adding a radio button for Amazon Web Services -->
+            <label for="aws">
                 <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
-                AWS @ $300
-              </label>
-              
-              <br/>
-              <br/>
-              
-              <label for="subtotal">
+                Amazon Web Services @ $300
+            </label>
+            <br/>
+            <br/>
+            
+            <!-- Adding a radio button for Cloud 9 -->
+            <label for="cloud9">
+                <input type="radio" id="cloud9" name="product" onClick="disablebtnProceed()"/>
+                Cloud 9 @ $200
+            </label>
+            <br/>
+            <br/>
+            
+            <!-- Adding a radio button for Gmail -->
+            <label for="Gmail">
+                <input type="radio" id="Gmail" name="product" onClick="disablebtnProceed()"/>
+                Gmail @ $400
+            </label>
+            <br/>
+            <br/>
+           
+            <!--Adding a textbox for the Subtotal  -->
+            <label for="subtotal">
                 Sub Total
                 <input type="text" id="subtotal" value="0.00" readonly/>
-              </label>
-              
-              <br/>
-              
-              <label for="total">
+            </label>
+            <br/>
+            <br/>
+            
+            <!-- Adding a textbox for the Discount  -->
+            <label for="discount">
+                Discount @ 5%
+                <input type="text" id="discount" name="discount" value="0.00" readonly/>
+            </label>
+            <br/>
+            <br/>
+            
+            <!-- Adding a textbox for the VAT  -->
+            <label for="vat">
+                VAT @ 10%
+                <input type="text" id="vat" name="vat" value="0.00" readonly/>
+            </label>
+            <br/>
+            <br/>
+           
+            <!-- Adding a textbox for the total -->
+            <label for="total">
                 Total
-                <input type="hidden" id="total" name="total" value="0.00" readonly/>
-              </label>
-    
-              <br/>
-              
-              <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+                <input type="text" id="total" name="total" value="0.00" readonly/>
+            </label>
+            <br/>
+            <br/>
+        
+            <!-- Adding the button to proceed -->
+            <button type="submit" id="btnProceed" class="button" disabled>Add to Shopping Cart</button>
+            <br/>
+            <br/>
             
-            </form>
-            
+        </form>
             <br/>
             <button onClick="calcSub()">Calculate Cost</button>
             <a role="button" href="Ebus1.php">Clear Choice</a>
--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -4,28 +4,55 @@ function calcSub(){
     
     var argSubTotal;
     
-    if(document.getElementById("salesforce").checked) {
-    argSubTotal = 100;
+if(document.getElementById("salesforce").checked){
+        argSubTotal = 100; 
+    }
+    else if(document.getElementById("aws").checked){
+        argSubTotal = 300; 
+    }
+     else if(document.getElementById("cloud9").checked){
+        argSubTotal = 200; 
     }
     else {
-    argSubTotal = 300;
+        argSubTotal = 400;
     }
+    calcDisVatTotal(argSubTotal); 
+}
 
-    display(argSubTotal);
-    }
+//Calculates the discount and vat 
+function calcDisVatTotal(parmSubTotal){
     
-    function display(parm1){
-        
-        document.getElementById("subtotal").value = parm1;
-        document.getElementById("total").value = parm1;
-        
-        enablebtnProceed();
-    }
+    //Declaing the variables
+    var subTotal;
+    var discountAmt;
+    var vatAmt;
+    var totalPrice;
     
-    function enablebtnProceed() {
-        $('#btnProceed').prop('disabled',false);
-    }
+    subTotal = parmSubTotal; //Pass the parameter value into a variable
+    discountAmt = (subTotal * 0.05); //Calculate the discount
+    vatAmt = ((subTotal - discountAmt) * 0.1); //Calculate the vat
+    totalPrice = (subTotal - discountAmt + vatAmt ); //Calculate the total
+    display(subTotal, discountAmt, vatAmt, totalPrice); //Pass the value to complete the function display() next
+}
+
+//Function to display the prices in the textboxes on the Ebus page
+function display(parm1, parm2, parm3, parm4)
+{
     
-    function disablebtnProceed() {
-        $('#btnProceed').prop('disabled',true);
-    }
+    document.getElementById("subtotal").value = parm1; //Display subtotal
+    document.getElementById("discount").value = parm2; //Display discount
+    document.getElementById("vat").value = parm3; //Display vat
+    document.getElementById("total").value = parm4; //Display total
+    
+    enablebtnProceed();
+}
+
+//Function that enables the proceed button
+function enablebtnProceed(){
+    $("#btnProceed").prop("disabled", false);
+}
+
+//Function that disable the proceed button
+function disablebtnProceed(){
+    $("#btnProceed").prop("disabled", true);
+}
